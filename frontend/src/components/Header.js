204b3c458047b21a1faf8b75e97072f1c09ee3b0
hieuphamminh976@gmail.com <hieuphamminh976@gmail.com>
2024-08-06 12:16:02 +0700

    add frontend

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Logo from './Logo'
+
+const Header = () => {
+  return (
+    <header className='h-16 shadow-md'>
+        <div className='h-full container mx-auto flex items-center px-4'>
+            <div className=''>
+                <Logo w={90} h={50}/>
+            </div>
+        
+            <div>
+                
+            </div>
+        </div>
+    </header>    
+  )
+}
+
+export default Header
