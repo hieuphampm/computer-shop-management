46c6412f7461e94bd3c9b792901a1f674c486743
hieuphampm <hieuphamminh976@gmail.com>
2024-08-07 15:36:16 +0700

    update

--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -6,9 +6,13 @@ import { Link } from 'react-router-dom';
 
 const SignUp = () => {
   const [showPassword,setShowPassword] = useState(false)
-    const [data,setData] = useState({
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false)
+  const [data,setData] = useState({
         email : "",
-        password : ""
+        password : "",
+        confirmPassword : "",
+        profilePic : "",
+
     })
 
     const handleOnChange = (e) =>{
@@ -35,10 +39,15 @@ const SignUp = () => {
 
             <div className='bg-white p-5 w-full max-w-sm mx-auto'>
                 <div className='w-20 h-20 mx-auto '>
-                    <img src={loginIcons} alt='login icons'/>
+                    <div>
+                      <img src={loginIcons} alt='login icons'/>
+                    </div>
+                    <div className='text-xs bg-slate-200 py-4 text-center'>
+                      Upload Photo
+                    </div>
                 </div>
 
-                <form className='pt-6' onSubmit={handleSubmit}>  
+                <form className='pt-6 flex flex-col gap-2' onSubmit={handleSubmit}>  
                     <div className='grid'>
                             <label>Name:</label>
                             <div className='bg-slate-100 p-2 '>
@@ -46,7 +55,7 @@ const SignUp = () => {
                                     type='text' 
                                     placeholder='enter your name'
                                     name='name'
-                                    value={data.email} 
+                                    value={data.name} 
                                     onChange={handleOnChange}
                                     className='w-full h-full outline-none bg-transparent'/>
                             </div>
@@ -90,15 +99,39 @@ const SignUp = () => {
                                 </span>
                             </div>
                         </div>
-                        <Link to ={'/forgot-password' } className='block w-fit ml-auto hover:underline hover:text-red-600'>
-                                Forgot password
-                        </Link>
                     </div>
 
-                    <button className='bg-red-600 hover:bg-red-700 text-white px-6 py-2 w-full max-w-[150px] rounded-full hover:scale-110 transition-all mx-auto block mt-6'>Login</button>
+                    <div>
+                        <label>Confirm Password:</label>
+                        <div className='bg-slate-100 p-2 flex'>
+                            <input 
+                                type={showConfirmPassword ? "text":"password"} 
+                                placeholder='confirm password'
+                                name='confirmPassword'
+                                value={data.confirmPassword} 
+                                onChange={handleOnChange}
+                                className='w-full h-full outline-none bg-transparent'/>
+                            <div className='cursor-pointer text-xl' onClick={()=>setShowConfirmPassword((preve)=>!preve)}>
+                                <span>
+                                    {
+                                        showConfirmPassword ? (
+                                            <IoIosEyeOff />
+                                        )
+                                        :
+                                        (
+                                            <IoIosEye />
+                                        )
+                                    }
+                                                                      
+                                </span>
+                            </div>
+                        </div>                 
+                    </div> 
+
+                    <button className='bg-red-600 hover:bg-red-700 text-white px-6 py-2 w-full max-w-[150px] rounded-full hover:scale-110 transition-all mx-auto block mt-6'>Sign Up</button>
                 </form>
 
-                <p className='my-5'>Don't have account?<Link to={"/sign-up"} className='text-red-600 hover:text-red-700 hover:underline'>Sign Up</Link></p>
+                <p className='my-5'>Already have account? <Link to={"/login"} className='text-red-600 hover:text-red-700 hover:underline'>Login</Link></p>
             </div>
 
         </div>
