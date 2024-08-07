a0cde23dc7b9ae93644a8e22407468d9ac2e0366
hieuphamminh976@gmail.com <hieuphamminh976@gmail.com>
2024-08-07 12:53:40 +0700

    update login,signup

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,13 +2,17 @@ import React from 'react'
 import Logo from './Logo'
 import { GrSearch } from "react-icons/gr";
 import { FaRegCircleUser } from "react-icons/fa6";
+import { FaCartShopping } from "react-icons/fa6";
+import {Link} from "react-router-dom"
 
 const Header = () => {
   return (
-    <header className='h-16 shadow-md'>
+    <header className='h-16 shadow-md bg-white'>
         <div className='h-full container mx-auto flex items-center px-4 justify-between'>
             <div className=''>
-                <Logo w={90} h={50}/>
+                <Link>
+                  <Logo w={90} h={50}/>
+                </Link>
             </div>
         
             <div className='hidden lg:flex items-center w-full justify-between max-w-sm border rounded-full focus-within:shadow-md pl-2'>
@@ -18,8 +22,23 @@ const Header = () => {
                 </div>
             </div>
 
-            <div>
-                <FaRegCircleUser />
+            <div className='flex items-center gap-4'>
+
+                <div className='text-3xl cursor-pointer'>
+                  <FaRegCircleUser />
+                </div>
+
+                <div className='text-2xl relative'>
+                  <span><FaCartShopping /></span>
+                  <div className='bg-red-600 text-white w-5 h-5 rounded-full p-1 flex items-center justify-center absolute -top-2 -right-3'>
+                    <p className='text-sm'>0</p>
+                  </div>
+                </div>
+
+                <div>
+                  <Link to={"/login"} className='px-3 py-1 rounded-full text-white bg-red-600 hover:bg-red-700'>Login</Link>
+                </div>
+
             </div>
         </div>
     </header>    
